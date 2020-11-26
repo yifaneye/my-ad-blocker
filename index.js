6a2c9dcffae9dfd7fa529430f8426805ab024170
Yifan Ai <ayfallen@hotmail.com>
2020-11-26 23:50:16 +1100

    Refactor code, minimize and collapse ads

--- index.js
+++ index.js
@@ -23,13 +23,3 @@ chrome.webRequest.onBeforeRequest.addListener(
 	},
 	["blocking"]
 );
-
-(function set_show_iframe_listeners() {
-	let iframes = document.querySelectorAll('iframe');
-	iframes.forEach(iframe => {
-			iframe.addEventListener('click', () => {
-				iframe.focus();
-			});
-		}
-	);
-})();
